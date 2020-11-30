6c2e7c04889ceee77d908cbabe9a4e79984a2432
Michael Olawunmi <molawunmi21@gmail.com>
2020-11-29 23:40:08 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const texts = ["Hi, I'm Michael And This Is My Dev Portfolio."]
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+ 
+(function type(){    
+    
+    if(count === texts.length){
+        count = 0;
+    }    
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.port-name').textContent = letter;
+        if(letter.length === texts.length) {
+            count++ ;
+    }
+    setTimeout(type, 215);
+}());
